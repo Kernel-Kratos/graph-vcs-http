81297d08cefeb9ef554126b28ff1096ce9839360
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-03-14 00:39:12 +0530

    Basic file structure and dependency

---
+++ src/main/java/com/neo4j/GitInsideOutApplication.java
@@ -0,0 +1,13 @@
+package com.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitInsideOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitInsideOutApplication.class, args);
+	}
+
+}
