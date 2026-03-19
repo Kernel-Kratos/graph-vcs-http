35bd840ac67d72ff339561d3912558de280831da
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-03-19 12:42:34 +0530

    Add CommitIngestion Service

---
+++ src/main/java/com/neo4j/service/commit/CommitIngestionService.java
@@ -0,0 +1,11 @@
+package com.neo4j.service.commit;
+
+import com.neo4j.node.Commit;
+
+public class CommitIngestionService {
+
+    //step 1:- Resolve repos;
+    //step 2:- process the files or blobs
+    //step 3
+
+}
---
+++ src/main/java/com/neo4j/service/commit/ICommitIngestionService.java
@@ -0,0 +1,8 @@
+package com.neo4j.service.commit;
+
+import com.neo4j.node.Commit;
+
+public interface ICommitIngestionService {
+
+
+}
