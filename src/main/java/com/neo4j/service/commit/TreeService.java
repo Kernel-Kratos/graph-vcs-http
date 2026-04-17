9cd99d7d62f5048695ff94d9845bcbaa2a78567d
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-04-18 00:44:28 +0530

    Add the recursive hashing algorithm for blobs and subsequently tree

--- src/main/java/com/neo4j/service/commit/TreeService.java
+++ src/main/java/com/neo4j/service/commit/TreeService.java
@@ -1,15 +1,17 @@
 package com.neo4j.service.commit;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.neo4j.node.FileBlob;
 import com.neo4j.node.Tree;
+import com.neo4j.repository.TreeRepository;
 import com.neo4j.utils.HashUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class TreeService {
+    private final TreeRepository treeRepository;
     //implement a feature in controller which throws error if filepaths.length
     public Tree treeRepresentation (List<String> filePaths, List<MultipartFile> file){
 
@@ -26,10 +29,9 @@ public class TreeService {
         rootTree.setFileBlob(new ArrayList<>());
 
         for (int i = 0; i < filePaths.size(); i++ ) {
-            //String filePath = filePaths[i];
             Tree currentNode = rootTree;
             String[] directoryNodes = filePaths.get(i).split("/");
-            for (int j = 0; j < directoryNodes.length - 1; i++) {
+            for (int j = 0; j < directoryNodes.length - 1; j++) {
                 //rewrite this using guard clause(later)
                 String temp = directoryNodes[j]; //to trick the compiler into thinking this is the final variable.
                 // this is becuase whenever a lamda is used in java it is neccessary for any outside variable to be final and java can't guarentee
@@ -40,7 +42,7 @@ public class TreeService {
                         .orElse(null);
                 if (matchingTree == null){
                     Tree newDirectory = new Tree();
-                    newDirectory.setFolderName(directoryNodes[i]);
+                    newDirectory.setFolderName(directoryNodes[j]);
                     newDirectory.setSubTrees(new ArrayList<>());
                     newDirectory.setFileBlob(new ArrayList<>());
                     currentNode.getSubTrees().add(newDirectory);
@@ -74,8 +76,9 @@ public class TreeService {
                     throw new RuntimeException("Error", e);
                 }
             }
-            
         }
+        rootTree = hashingIt(rootTree);
+        treeRepository.save(rootTree);
         return rootTree;
     }
     // why is rootTree = currentNode not done before returning rootTree?
@@ -84,4 +87,42 @@ public class TreeService {
     then i initialized the subtrees and fileBlob to arrayList this means, rootTree was also initialized
     similarly when I added subtrees and fileBlobs to list, rootTree was also updated and there is no need to save it again.
     Only difference is rootTree points to / and currentNode will point to whatever the lastest addition was  */
+
+
+    public static Tree hashingIt (Tree tree) {
+        for (Tree currentNode : tree.getSubTrees()) {
+            hashingIt(currentNode);
+        }
+        for (FileBlob blob : tree.getFileBlob()){
+                try{
+                blob.setHashId(HashUtil.hashBlob(blob.getRawContent()));
+                } catch (NoSuchAlgorithmException e){
+                    throw new RuntimeException(e);
+                }
+        }
+        
+        // In Merkle DAG, parent node is generated acc to the hashes of its children.
+        // Below i made a new ArrayList childhashes which will store the hashes of "current children of node"
+        //eg :- java/hello/hell/hi.java
+        //we are on hello then this list will store : hash of hi.java, hell and the generate a hash based on this.
+        List<String> childHashes = new ArrayList<>();
+
+        for (FileBlob blob : tree.getFileBlob()){
+            childHashes.add("blob:" + blob.getHashId());
+        }
+
+        for (Tree subTree : tree.getSubTrees()){
+            childHashes.add("tree:" + subTree.getHashId());
+        }
+
+        Collections.sort(childHashes);
+
+        String commbinedHahses = String.join("", childHashes);
+        tree.setHashId(HashUtil.hashString(commbinedHahses));
+        return tree;
+    }
+
+    // added "tree" & "blob" to avoid collison and re-wrtiing.
+    // because if a file's hash ends up being 08732... 
+    // and the folder's hash ends uo being same 0832.. this will lead to collison and possibly rewrite.
 }
