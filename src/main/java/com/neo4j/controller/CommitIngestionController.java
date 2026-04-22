d5ae3f05974c82eebce9e6e15fd0ec034d0ad2d3
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-04-23 01:53:10 +0530

    Added support for depository and branch layer

--- src/main/java/com/neo4j/controller/CommitIngestionController.java
+++ src/main/java/com/neo4j/controller/CommitIngestionController.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.neo4j.dto.CommitDto;
+import com.neo4j.dto.DepositoryDto;
 import com.neo4j.node.Commit;
+import com.neo4j.node.Depository;
 import com.neo4j.service.commit.CommitIngestionService;
+import com.neo4j.service.commit.DepositoryService;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -22,14 +26,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 public class CommitIngestionController {
     private final CommitIngestionService commitIngestionService;
+    private final DepositoryService depositoryService;
 
-    @PostMapping("")
-    public ResponseEntity<CommitDto> commitIngestion (@RequestParam List<String> filePath, @RequestParam List <MultipartFile> file,
-        @RequestParam String author, @RequestParam String email, @RequestParam String message, @RequestParam(required = false) String parentHash ){
+    @PostMapping("/{repoName}/{branchName}")
+    public ResponseEntity<DepositoryDto> commitIngestion (@RequestParam List<String> filePath, @RequestParam List <MultipartFile> file,
+        @RequestParam String author, @RequestParam String email, @RequestParam String message, @RequestParam(required = false) String parentHash,
+    @PathVariable String repoName, @PathVariable String branchName){
         try {
         Commit commit = commitIngestionService.commitSave(filePath, file, author, email, message, parentHash);
         CommitDto commitDto = commitIngestionService.convertToCommitDto(commit);
-        return ResponseEntity.ok(commitDto);
+        Depository updatedRepo = depositoryService.addToDepository(repoName, branchName, commit);
+        DepositoryDto depositoryDto = depositoryService.convertTDepositoryDto(updatedRepo, commitDto);    
+        return ResponseEntity.ok(depositoryDto);
         } catch(Exception e) {
             throw new RuntimeException(e);
         }
