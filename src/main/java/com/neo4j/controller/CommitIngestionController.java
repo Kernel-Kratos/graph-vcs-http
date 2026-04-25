f2c26af331838553a4c0ceee274417fb170c1bd8
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-04-26 01:53:03 +0530

    Add check to match uploaded file with path

--- src/main/java/com/neo4j/controller/CommitIngestionController.java
+++ src/main/java/com/neo4j/controller/CommitIngestionController.java
@@ -32,6 +32,13 @@ public class CommitIngestionController {
     public ResponseEntity<DepositoryDto> commitIngestion (@RequestParam List<String> filePath, @RequestParam List <MultipartFile> file,
         @RequestParam String author, @RequestParam String email, @RequestParam String message, @RequestParam(required = false) String parentHash,
     @PathVariable String repoName, @PathVariable String branchName){
+        if (filePath.size() != file.size())
+            throw new RuntimeException("There are not equal number of file to corresponding to filepath");
+        for (int i = 0; i < filePath.size(); i++){
+            if (! filePath.get(i).endsWith(file.get(i).getOriginalFilename())){
+                throw new RuntimeException("There is mis-match in the filename in file path & filename of the blob");
+            }
+        }
         try {
         Commit commit = commitIngestionService.commitSave(filePath, file, author, email, message, parentHash);
         CommitDto commitDto = commitIngestionService.convertToCommitDto(commit);
@@ -45,3 +52,10 @@ public class CommitIngestionController {
     
 
 }
+//test hello.txt and hello1.txt when they have same content and also same path.
+//ans -> so basically 2 arrows are drawn from src which point 2 same blob.
+
+// do the variation in this, same path diff content 
+// ans-> 2 pointer from src and 2 diff blobs
+//same contnet diff path etc.
+//I send a filenamed hello2.txt but in the path the file'sname wqas diff it was hello.txt and this is what was pickedup.
