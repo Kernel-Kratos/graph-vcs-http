de2e48e2e8ad503b3a911e7a75508b4a8aa604b8
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-05-04 02:04:54 +0530

    Add custom query to update branch pointer

--- src/main/java/com/neo4j/repository/BranchRepository.java
+++ src/main/java/com/neo4j/repository/BranchRepository.java
@@ -1,9 +1,14 @@
 package com.neo4j.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import com.neo4j.node.Branch;
 
 public interface BranchRepository extends Neo4jRepository<Branch, String> {
+    @Query( "MATCH (b:Branch {branchName: $BranchName}) OPTIONAL MATCH (b)-[oldrel:POINTS_TO]->(c1:Commit) DELETE oldrel WITH b  MATCH (c2: Commit{hashId : $commitHash}) MERGE (b)-[newrel:POINTS_TO]->(c2) RETURN b;")
+    Optional<Branch> updateBrachPointer (String BranchName, String commitHash);
 
 }
