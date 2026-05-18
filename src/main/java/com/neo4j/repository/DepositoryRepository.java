60899847a99ef433dfc26d5f406ecaff3b63e1ec
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-05-19 01:57:25 +0530

    Add custom query for Has_Head relation

--- src/main/java/com/neo4j/repository/DepositoryRepository.java
+++ src/main/java/com/neo4j/repository/DepositoryRepository.java
@@ -1,9 +1,12 @@
 package com.neo4j.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import com.neo4j.node.Depository;
 
 public interface DepositoryRepository extends Neo4jRepository<Depository, String>{
+    @Query("MATCH (d:Depository {repoName: $depositoryName}) OPTIONAL MATCH (d)-[oldHead:HAS_HEAD]->() DELETE oldHead WITH d MATCH (c: Commit{hashId : $newHead}) MERGE (d)-[newHead:HAS_HEAD]->(c) RETURN d;")
+    void updateBranchHead (String depositoryName, String newHead);
 
 }
