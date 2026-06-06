34a785d68caec7c949df28608e93e5966c927ca4
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-06-07 02:21:59 +0530

    Update getAll metho to send zip file to client

--- src/main/java/com/neo4j/controller/CommitFetchController.java
+++ src/main/java/com/neo4j/controller/CommitFetchController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.springframework.http.MediaType;
